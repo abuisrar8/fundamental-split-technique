1e6eac11f06c120eb9df4001834622b172016404
ISRAR <abu.israr.khan.9@gmail.com>
2025-04-17 16:52:13 +0530

    Create __init__.py

---
+++ fst/__init__.py
@@ -0,0 +1,13 @@
+"""
+Fundamental Split Technique (FST)
+---------------------------------
+
+A robust and adaptive data partitioning algorithm for regression modeling.
+"""
+
+from .core import FST
+
+__version__ = '0.1.0'
+__author__ = 'Abu Israr'
+__email__ = 'abu.israr.khan.9@gmail.com'
+__all__ = ['FST']
